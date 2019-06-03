4a75ecaebd9e6e440682b4e906466d2c53bb7403
Jose Emmanuel Torrijos <jose.emmanuel.torrijos@gmail.com>
2019-06-04 02:52:16 +0800

    Add Patient search functionality

--- catclinic-server/src/main/java/com/joeltorrijos/catclinic/repository/PatientRepository.java
+++ catclinic-server/src/main/java/com/joeltorrijos/catclinic/repository/PatientRepository.java
@@ -1,11 +1,18 @@
 package com.joeltorrijos.catclinic.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.joeltorrijos.catclinic.model.Patient;
 
 @RepositoryRestResource
 public interface PatientRepository extends JpaRepository<Patient, Long> {
 
+	@RestResource(path="name")
+	List<Patient> findByFirstNameContainingOrLastNameContainingAllIgnoreCase(String firstName, String lastName, Pageable pageable);
+	
 }
