fc4b05c947bb391262f2862e75af8111efa0b764
Jose Emmanuel Torrijos <jose.emmanuel.torrijos@gmail.com>
2019-06-26 18:10:32 +0800

    Modify Production API url

--- catclinic-web/src/main/web/src/environments/environment.prod.ts
+++ catclinic-web/src/main/web/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  api_url: 'https:///localhost:' + process.env.PORT + '/api'
+  api_url: 'http:///localhost:8080/api'
   // api_url: 'https://radiant-basin-72035.herokuapp.com/api'
 };
